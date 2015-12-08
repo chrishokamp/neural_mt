205e7b5a30a080280398db6a363347bf5daf33cf
Chris Hokamp <chris.hokamp@gmail.com>
2015-12-08 15:15:52 +0000

    script to extract parameters from a trained model

---
+++ scripts/extract_parameters.py
@@ -0,0 +1,34 @@
+import os
+import re
+import argparse
+import logging
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+parser = argparse.ArgumentParser(
+    description="""Extract the parameters from a serialized blocks model""", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("model_file", type=str, help="The location of your serialized .npz model")
+
+
+# the model _MUST_ be loaded in the same python context (same modules available), otherwise there will be an error
+# also, if the model was trained on the GPU, the saved model must be loaded on the GPU
+def extract_model_parameters(config):
+
+    model = np.load(config['model_file'])
+
+    for param_name in model.keys():
+        param_value = model[param_name][()].get_value()
+        output_name = re.sub('/', '.', param_name) + '.npz'
+        output_path = os.path.join(extract_dir, output_name)
+
+        with open(output_path, 'wb') as output:
+            np.save(output, param_value)
+
+    logger.info('saved model key: {} to {}'.format(param_name, output_name))
+
+if __name__ == '__main__':
+    config = vars(parser.parse_args())
+    extract_model_parameters(config)

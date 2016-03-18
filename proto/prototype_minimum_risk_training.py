71fa9ecd42321f6a2cf6cc3ff2179ff4f014c8df
Chris Hokamp <chris.hokamp@gmail.com>
2016-03-18 09:04:41 +0000

    hotfix

--- proto/prototype_minimum_risk_training.py
+++ proto/prototype_minimum_risk_training.py
@@ -201,11 +201,11 @@ theano_sample_func = sample_model.get_theano_function()
 # TODO: or by avoiding the for loop somehow
 def sampling_func(source_seq, num_samples=1):
 
-    def _get_true_length(seqs, trg_vocab):
+    def _get_true_length(seqs, vocab):
         try:
             lens = []
             for r in seqs.tolist():
-                lens.append(r.index(trg_vocab['</S>']) + 1)
+                lens.append(r.index(vocab['</S>']) + 1)
             return lens
         except ValueError:
             return [seqs.shape[1] for _ in range(seqs.shape[0])]
@@ -222,13 +222,13 @@ def sampling_func(source_seq, num_samples=1):
         # outputs = outputs[:_get_true_length(outputs, trg_vocab)]
         # samples.append(outputs)
 
-    inputs = tensor.tile(source_seq[None, :], (num_samples, 1))
+    inputs = numpy.tile(source_seq[None, :], (num_samples, 1))
     # the output is [seq_len, batch]
     _1, outputs, _2, _3, costs = theano_sample_func(inputs)
     outputs = outputs.T
 
     # TODO: this step could be avoided by computing the samples mask in a different way
-    lens = _get_true_length(outputs)
+    lens = _get_true_length(outputs, trg_vocab)
     samples = [s[:l] for s,l in zip(outputs.tolist(), lens)]
 
     return samples
